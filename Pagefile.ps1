29d65c1a20f7faf3a7edd0002dc649b286507695
nitinpandey-ms <nitinpandey@microsoft.com>
2023-05-09 12:02:00 +1000

    Final

--- Pagefile.ps1
+++ Pagefile.ps1
@@ -10,8 +10,6 @@ Note that both the Pagefile and Memory Dump are INTENTIONALLY configured in the 
 
 User input and script progress logs are saved in C:\PagefileMemoryDumpConfiguration.log
 
-Testing
-
 #>
 
 #Creaet a Function and call it
